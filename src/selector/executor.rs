ac918e8b14f633d85b67708a038f314ac6de0060
Matthew Sutton <ziggymatt@epbfi.com>
2022-10-02 15:33:53 -0400

    Removed debug print

--- src/selector/executor.rs
+++ src/selector/executor.rs
@@ -4,13 +4,6 @@ pub fn execute(root_dir : PathBuf, exec_path : String) {
     let full_path = format!("{}\\{}\\{}",project_root::get_project_root().unwrap().display(),root_dir.display(),exec_path);
     //println!("{}",full_path);
     if exec_path.ends_with(".jar") {
-        let java = Command::new("cmd")
-            .args(&["/C","java -version"])
-            .output()
-            .expect("Failed to execute process");
-
-            println!("{:?}",String::from_utf8(java.stderr));
-
         let _output = Command::new("cmd")
             .args(&["/C",format!("java -jar {}", full_path).as_str()])
             .output()
