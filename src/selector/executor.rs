4e15d7a25bb6ea0cb633ad1239ff763f11c313c5
Matthew Sutton <ziggymatt@epbfi.com>
2022-10-02 15:56:37 -0400

    Fixed issues with directories and executor

--- src/selector/executor.rs
+++ src/selector/executor.rs
@@ -1,17 +1,22 @@
 use std::{path::PathBuf, process::Command};
 
 pub fn execute(root_dir : PathBuf, exec_path : String) {
-    let full_path = format!("{}\\{}\\{}",project_root::get_project_root().unwrap().display(),root_dir.display(),exec_path);
+    let full_path = format!("{}\\{}",project_root::get_project_root().unwrap().display(),root_dir.display());
     //println!("{}",full_path);
     if exec_path.ends_with(".jar") {
-        let _output = Command::new("cmd")
-            .args(&["/C",format!("java -jar {}", full_path).as_str()])
+
+        let output = Command::new("cmd")
+            .args(&["/C",format!("cd {} && java -jar {}", full_path, exec_path).as_str()])
             .output()
             .expect("Failed to execute process");
+
+        println!("{:?}",String::from_utf8(output.stderr));
     } else {
-        let _output = Command::new("cmd")
-            .args(&["/C",format!("{}", full_path).as_str()])
+        let output = Command::new("cmd")
+            .args(&["/C",format!("cd {} && {}", full_path, exec_path).as_str()])
             .output()
             .expect("Failed to execute process");
+            
+        println!("{:?}",String::from_utf8(output.stderr));
     }
 }
